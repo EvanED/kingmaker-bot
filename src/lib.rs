1c258848df0ced2b2c4bc7757b99a63cc375aed9
Evan Driscoll <evaned@gmail.com>
2023-09-08 01:45:36 -0500

    Remove last vestages of Cat

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,3 @@
 pub mod spec;
 pub mod roll_context;
 pub mod roll_result;
-
-#[derive(Debug, Default)]
-pub struct Cat {
-    pub hungry: bool,
-}
-
-impl Cat {
-    pub fn feed(&mut self) {
-        self.hungry = false;
-    }
-}
